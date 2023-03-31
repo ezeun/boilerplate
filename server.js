be5f65d5088619569fe8f4ce9e81b0af4b6a01ce
ezeun <consistency1231@gmail.com>
2023-03-31 15:48:19 +0900

    GET, POST, DELETE

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
+
+var db;
+MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.ecwllpn.mongodb.net/?retryWrites=true&w=majority', function(error, client){
+    if(error) return console.log(error)
+    db = client.db('compiler');
+
+    // db.collection('post').insertOne({이름 : 'leeju', _id : 100}, function(error, res){
+    //     console.log('저장완료');
+    // });
+
+    app.listen(8080, function(){ 
+        console.log('listening on 8080')
+    });    
+});
+
+app.get('/pet', function(req, res){ 
+    res.send('펫 용품 쇼핑 페이지입니다.')
+});
+
+app.get('/beauty', function(req, res){
+    res.send('뷰티 용품 쇼핑 페이지입니다.')
+});  
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+});  
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html')
+});  
+
+app.post('/add', function(req, res){
+    res.send('전송완료')
+    console.log(req.body)
+    
+    console.log(req.body.code)
+    console.log(req.body.input)
+    console.log(req.body.output)
+
+    db.collection('counter').findOne({name : '게시물 개수'}, function(error, result){
+        console.log(result.totalPost); 
+        var postNum = result.totalPost;
+
+        db.collection('post').insertOne({_id : postNum + 1, 코드 : req.body.code, 입력 : req.body.input, 출력 : req.body.output}, function(){
+            console.log('저장완료');
+            db.collection('counter').updateOne({name : '게시물 개수'}, { $inc : {totalPost : 1}}, function(error, result){
+                if(error) return console.log(error)
+            })
+        });
+    });
+
+});
+
+app.get('/list', function(req, res){
+    db.collection('post').find().toArray(function(error, result){ 
+        console.log(result);
+        res.render('list.ejs', { posts : result}); 
+    }); 
+});  
+
+app.delete('/delete', function(req, res){
+    console.log(req.body);
+    req.body._id = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, function(error, result){
+        console.log('삭제완료');
+        res.status(200).send({ message : '성공했습니다'});
+    })
+});
+
+app.get('/detail/:id', function(req, res){
+    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(error, result){ 
+        console.log(result)
+        res.render('detail.ejs', { data : result}); 
+    })
+}); 
